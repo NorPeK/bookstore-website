1e1d344053c8d07245150b42fb5e384efb6f5a8b
NorPeK <norpekbusiness@hotmail.com>
2024-10-21 12:56:33 +0300

    adding imports

--- backend/index.js
+++ backend/index.js
@@ -12,14 +12,14 @@ app.use(express.json()); // Middleware to parse incoming JSON requests to JS
 
 
 // Option 1: Allow all origins with default of cors(*)
-//app.use(cors());
+app.use(cors());
 
 // Option 2: Allow Custom Origins
-app.use(cors({
-    origin: 'http://localhost:3000',
-    methods: ['GET' , 'POST' , 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type'],
-}))
+// app.use(cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET' , 'POST' , 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+// }))
 
 // Route to handle GET requests to the root URL
 app.get('/', (request, response) => {
