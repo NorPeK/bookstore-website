9d293798b99d20a5a9bd072fc407cac9000f690e
NorPeK <norpekbusiness@hotmail.com>
2024-10-27 16:33:27 +0300

    Card component

---
+++ frontend/src/components/home/BookModal.jsx
@@ -0,0 +1,14 @@
+import { AiOutlineClose } from 'react-icons/ai'
+import { PiBookOpenTextLight } from 'react-icons/pi'
+
+
+
+import React from 'react'
+
+const BookModal = () => {
+  return (
+    <div>BookModal</div>
+  )
+}
+
+export default BookModal
